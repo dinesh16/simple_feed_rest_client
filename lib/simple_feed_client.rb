3f80393837f0b743aa7701b85bf317549759a955
Dinesh Anthony <dinesh.anthony@surfdome.com>
2014-01-31 10:45:10 +0000

    initial commit

---
+++ lib/simple_feed_client.rb
@@ -0,0 +1,29 @@
+require 'rest_client'
+
+class SimpleFeedClient
+
+  attr_reader :username, :password
+
+  def initialize(params)
+    @username = params[:username]
+    @password = params[:password]
+  end
+
+  def request
+    # pry
+    # binding.pry
+    RestClient::Request.execute(:url => 'http://localhost:3000/api/feeds/dinesh16/items.json', 
+                                :method => :get, 
+                                :user =>@username, 
+                                :password => @password)
+  end
+
+  class Feed
+
+  end
+
+  class User
+
+  end
+
+end
---
+++ spec/simple_feed_client_spec.rb
@@ -0,0 +1,74 @@
+require './lib/simple_feed_client'
+require 'json'
+
+
+
+describe SimpleFeedClient do 
+
+  let!(:client) {
+    client = SimpleFeedClient.new(username: "dinesh16", password: "passwd")
+  }
+
+  it "has username" do
+    expect(client.username).to eq("dinesh16")
+  end
+
+  it "has password" do
+    expect(client.password).to eq("passwd")
+  end
+
+  it "has type" do
+    type = JSON.parse(client.request).first["type"]
+    expect(type).to eq("ImageItem")
+  end
+
+  it "has image_url" do
+    image_url = JSON.parse(client.request).first["image_url"]
+    expect(image_url).to eq("http://www.newscientist.com/blogs/shortsharpscience/assets_c/2011/07/SelfMonkey-thumb-600x723-131986.jpg")
+  end
+
+  it "has text" do
+    text = JSON.parse(client.request).first["text"]
+    expect(text).to eq("Hi guys whats up")
+  end
+
+  it "has id" do
+    id = JSON.parse(client.request).first["id"]
+    expect(id).to eq(45)
+  end
+
+  it "has created at" do
+    created_at = JSON.parse(client.request).first["created_at"]
+    expect(created_at).to eq("2014-01-30T23:20:12Z")
+  end
+
+  it "has link" do
+    link = JSON.parse(client.request).first["link"]
+    expect(link).to eq("http://localhost:3000/api/feeds/dinesh16/items/45.json")
+  end
+
+  it "has feed" do
+    pending "do it later"
+    feed = JSON.parse(client.request).first["feed"]
+    expect(feed).to eq("feeddddd")
+  end
+
+  it "has user" do
+    pending "do it later"
+    created_at = JSON.parse(client.request).first["created_at"]
+    # expect(created_at).to eq("2014-01-30T23:20:12Z")
+  end
+
+  # "type":"ImageItem",
+  # "image_url":"http://www.newscientist.com/blogs/shortsharpscience/assets_c/2011/07/SelfMonkey-thumb-600x723-131986.jpg",
+  # "text":"Hi guys whats up",
+  # "id":45,
+  # "created_at":"2014-01-30T23:20:12Z",
+  # "link":"http://localhost:3000/api/feeds/dinesh16/items/45.json",
+  # "feed":{"link":"http://localhost:3000/api/feeds/dinesh16.json",
+  #         "name":"dinesh16"},
+  # "user":{"username":"dinesh16",
+  #         "email":"dinesh.anthony@surfdome.com",
+  #         "gravatar":"http://www.gravatar.com/avatar/55d5e747fc442f493b00fc1179227c5a?s=40"
+
+end
